2e04c62947271635e61069347c6889f091d09e43
tienminhdn <tienminhdn97@gmail.com>
2021-05-20 10:35:59 +0700

    base

---
+++ DemoApp/Define/App.swift
@@ -0,0 +1,16 @@
+//
+//  App.swift
+//  DemoApp
+//
+//  Created by T.Minh on 5/20/21.
+//
+
+import Foundation
+import UIKit
+
+struct App {
+    struct Color {
+        static let appColor = UIColor.RGB(247, 56, 31)
+        static let greyColor = UIColor.RGB(242, 242, 247)
+    }
+}
---
+++ DemoApp/Define/Strings.swift
@@ -0,0 +1,16 @@
+//
+//  Strings.swift
+//  DemoApp
+//
+//  Created by T.Minh on 5/20/21.
+//
+
+import Foundation
+extension App {
+    
+    struct String {
+       static let String1 = "define string"
+    }
+}
+
+extension App.String { }
